d3778ec4848d61c67ca76259789b392d02ed4111
Rogerio Angeliski <angeliski@hotmail.com>
2019-01-08 21:32:13 -0200

    WIP - Add the main component as simple as possible

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,41 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <VueTabulator v-model="data" :options="options"/>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from './components/HelloWorld.vue';
+import VueTabulator from './components/VueTabulator.vue';
 
 @Component({
   components: {
-    HelloWorld,
+    VueTabulator,
   },
 })
-export default class App extends Vue {}
+export default class App extends Vue {
+  public data = [{ name: 'Teste' }];
+
+  public options = {
+    columns: [
+      {
+        title: 'Name',
+        field: 'name',
+        sorter: 'string',
+        width: 200,
+        editor: true,
+      },
+    ],
+  };
+}
 </script>
 
 <style lang="scss">
+@import "./scss/bootstrap/tabulator_bootstrap4";
+
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
---
+++ src/components/VueTabulator.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="tabulator" ref="table" v-if="tabulatorInstance"></div>
+</template>
+
+
+<script lang='ts'>
+import { Component, Prop, Vue } from 'vue-property-decorator';
+import Tabulator from 'tabulator-tables';
+
+@Component
+export default class VueTabulator extends Vue {
+  @Prop({ default: () => [] })
+  public value?: any[];
+
+  @Prop({ default: () => [] })
+  public tableData?: any[];
+
+  private tabulatorInstance: any = {};
+
+  @Prop({ default: () => ({}) })
+  private options: any;
+
+  private resolvedOptions: any = {};
+
+  mounted() {
+    this.resolvedOptions = {
+      ...this.options,
+      data: this.value,
+    };
+
+    this.tabulatorInstance = new Tabulator(
+      this.$refs.table,
+      this.resolvedOptions,
+    );
+  }
+}
+</script>
+
+
+<style lang='scss'>
+</style>

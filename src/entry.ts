e0cc9eed903e116c3f1a23416fce5da0ebb9ce91
Rogerio Angeliski <angeliski@hotmail.com>
2019-02-07 19:33:18 -0200

    Export component

--- src/entry.ts
+++ src/entry.ts
@@ -1,6 +1,8 @@
 import Vue from 'vue';
 import VueTabulator from '@/components/VueTabulator.vue';
 
+export { VueTabulator };
+
 export default {
     install(VueApp: typeof Vue) {
       VueApp.component(VueTabulator.name, VueTabulator);

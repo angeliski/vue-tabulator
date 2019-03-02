55d3e5767754fce27b543a48c06fa4d4c36a7b5a
Rogerio Angeliski <angeliski@hotmail.com>
2019-03-02 15:37:28 -0300

    Update configs to build lib without Tabulator

--- vue.config.js
+++ vue.config.js
@@ -2,12 +2,24 @@ const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   lintOnSave: true,
+  chainWebpack: (config) => {
+    const tabulator = {
+      'tabulator-tables': {
+        commonjs: 'tabulator-tables',
+        commonjs2: 'tabulator-tables',
+        amd: 'tabulator-tables',
+        root: 'Tabulator',
+      },
+    };
+
+    config.merge({
+      externals: process.env.NODE_ENV === 'production' ? tabulator : [],
+    });
+  },
   configureWebpack: {
     output: {
       libraryExport: 'default',
     },
-    plugins: [
-      new CopyWebpackPlugin([{ from: 'src/scss', to: 'scss' }]),
-    ],
+    plugins: [new CopyWebpackPlugin([{ from: 'src/scss', to: 'scss' }])],
   },
 };

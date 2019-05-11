23c869929fa2d26f10ba18e24f9b8cbcd1fc1bad
Rogerio Angeliski <angeliski@hotmail.com>
2019-05-11 20:33:55 -0300

    Try generate ds.t automatic

--- vue.config.js
+++ vue.config.js
@@ -1,22 +1,44 @@
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-  lintOnSave: true,
-  chainWebpack: (config) => {
-    const tabulator = {
-      'tabulator-tables': {
-        commonjs: 'tabulator-tables',
-        commonjs2: 'tabulator-tables',
-        amd: 'tabulator-tables',
-        root: 'Tabulator',
-      },
-    };
+  chainWebpack: config => {
+    if(process.env.NODE_ENV === 'production') {
+      // disable cache (not sure if this is actually useful...)
+      config.module.rule("ts").uses.delete("cache-loader");
 
-    config.merge({
-      externals: process.env.NODE_ENV === 'production' ? tabulator : [],
-    });
+      config.module
+        .rule('ts')
+        .use('ts-loader')
+        .loader('ts-loader')
+        .tap(opts => {
+          opts.transpileOnly = false;
+          opts.happyPackMode = false;
+          return opts;
+        });
+
+        const tabulator = {
+          'tabulator-tables': {
+            commonjs: 'tabulator-tables',
+            commonjs2: 'tabulator-tables',
+            amd: 'tabulator-tables',
+            root: 'Tabulator',
+          },
+        };
+    
+        config.merge({
+          externals: tabulator,
+        });
+    }
   },
-  configureWebpack: {
-    plugins: [new CopyWebpackPlugin([{ from: 'src/scss', to: 'scss' }])],
+  configureWebpack: config => {
+    if(process.env.NODE_ENV === 'production') {
+      config.module.rules.forEach(rule => {
+        if (rule.use) {
+          let idx = rule.use.findIndex(w => w.loader === 'thread-loader')
+          if (idx !== -1) rule.use.splice(idx, 1)
+        }
+      })
+    }
+    // plugins: [new CopyWebpackPlugin([{ from: 'src/scss', to: 'scss' }])],
   },
 };

61b0f349818333877a01e265402999dabf4890ca
biker2000on <bikerhiker10@gmail.com>
2019-08-09 08:06:21 +0400

    chore: fix linting issues

--- src/feature/event-factory.ts
+++ src/feature/event-factory.ts
@@ -1,13 +1,13 @@
-import Vue from 'vue'
-import dashify from 'dashify'
+import Vue from 'vue';
+import dashify from 'dashify';
 
 export default function (this: Vue, events: string[]): Object {
-  const vm = this
-  let obj: {[index:string] : {}} = {}
-  events.forEach(event => {
-    obj[event] = function(...args: any[]) {
-      vm.$emit(dashify(event),...args) 
-    }
-  })
-  return obj
+  const vm = this;
+  const obj: {[index:string] : {}} = {};
+  events.forEach((event) => {
+    obj[event] = (...args: any[]) => {
+      vm.$emit(dashify(event), ...args);
+    };
+  });
+  return obj;
 }
--- src/utilities/merge.ts
+++ src/utilities/merge.ts
@@ -1,8 +1,8 @@
-import joint from './joint'
+import joint from './joint';
 
 export default function merge(objValue: any, srcValue: any, key: string): any | undefined {
-  if (typeof objValue == "function" && typeof srcValue == "function") {
-    return joint([objValue, srcValue])
+  if (typeof objValue === 'function' && typeof srcValue === 'function') {
+    return joint([objValue, srcValue]);
   }
-  return
+  return undefined;
 }

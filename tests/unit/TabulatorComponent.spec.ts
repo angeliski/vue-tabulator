b59eba467ad0bbfa3a102a545203e25e3295ad9a
Rogerio Angeliski <rogerio.angeliski@resultadosdigitais.com.br>
2020-08-06 21:30:46 -0300

    fix: resolve options object issue
    The reference in the watcher function is reclyced between updates, now the object is new and
    isolated
    
    fix #44

--- tests/unit/TabulatorComponent.spec.ts
+++ tests/unit/TabulatorComponent.spec.ts
@@ -1,7 +1,7 @@
 import { shallowMount, mount } from '@vue/test-utils';
 import TabulatorComponent from '@/components/TabulatorComponent.vue';
-import { getInstance } from './helpers';
 import Vue from 'vue';
+import { getInstance } from './helpers';
 
 describe('TabulatorComponent.vue', () => {
   describe('Init table', () => {
@@ -65,6 +65,50 @@ describe('TabulatorComponent.vue', () => {
       const currentInstance = getInstance(wrapper);
 
       expect(oldInstance).not.toBe(currentInstance);
+    });
+
+    test('update the config should remove the old column the table', async () => {
+      const columns : Tabulator.ColumnDefinition[] = [
+        {
+          title: 'Name',
+          field: 'name',
+          sorter: 'string',
+          width: 200,
+          editor: true,
+        },
+        {
+          title: 'Age',
+          field: 'age',
+          sorter: 'string',
+          width: 200,
+          editor: true,
+        },
+      ];
+      const options : Tabulator.Options = {
+        columns,
+      };
+
+      const wrapper = mount(TabulatorComponent, {
+        propsData: {
+          options,
+        },
+      });
+
+
+      const oldInstance = getInstance(wrapper);
+      wrapper.setProps({
+        options: {
+          columns: [columns[0]],
+        },
+      });
+      await Vue.nextTick();
+      const currentInstance = getInstance(wrapper);
+
+
+      expect(oldInstance.getColumns()).not.toBe(currentInstance.getColumns());
+
+      expect(currentInstance.getColumns().length).toBe(1);
+      expect(currentInstance.getColumns()[0].getDefinition().title).toBe('Name');
     });
 
     test('update the v-model not should recreate the table', () => {
